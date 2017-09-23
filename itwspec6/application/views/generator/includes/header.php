9e8fc816b21204da528e817f2ab0337cfa69dffd
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2017-09-23 23:31:30 +0800

    eto
    bago to

---
+++ itwspec6/application/views/generator/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel ="stylesheet" href ="<?= $this->config->base_url()?>assets/css/bootstrap.css">
+    </head>
+    <body>
+  
