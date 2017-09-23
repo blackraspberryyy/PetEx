5bf85e02775ea5ab953280b5fc4b91fe12ca7cec
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2017-09-24 00:04:50 +0800

    Tanggal
    Tinanggal ko na pre ha

--- itwspec6/application/views/generator/includes/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel ="stylesheet" href ="<?= $this->config->base_url()?>assets/css/bootstrap.css">
-    </head>
-    <body>
-  
--- itwspec6/application/views/newcontroller/function2.php
+++
@@ -1,1 +0,0 @@
-<h1>This is Function 2!!</h1>
