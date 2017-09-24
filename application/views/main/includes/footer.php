a3f1c73a6365d8896c0404587633a7b8c52cce59
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2017-09-24 23:08:06 +0800

    Index
    Landing Page

---
+++ application/views/main/includes/footer.php
@@ -0,0 +1,30 @@
+
+    <a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bgdark icon-2x"></i></a>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.scrollTo.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.nav.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.localscroll-1.2.7-min.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/bootstrap.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.prettyPhoto.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/isotope.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/jquery.flexslider.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/inview.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/animate.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/js/custom.js"></script>
+    <script src="<?= $this->config->base_url()?>assets/contactform/contactform.js"></script>
+    <script>(function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-55234356-4', 'auto');
+        ga('send', 'pageview');
+    </script>
+    </body>
+    </html>
---
+++ application/views/main/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href ="<?= $this->config->base_url()?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href ="<?= $this->config->base_url()?>assets/css/style.css" rel="stylesheet">
+        <link href="<?= $this->config->base_url()?>assets/color/default.css" rel="stylesheet">
+        <link rel="shortcut icon" href="<?= $this->config->base_url()?>images/img/favicon.ico">
+    </head>
+    <body>
