9f9210846b0380726b763b9621933a89251439c4
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2017-09-24 23:04:32 +0800

    landingpage
    ....

--- application/views/main/includes/header.php
+++ application/views/main/includes/header.php
@@ -1,31 +1,14 @@
-<html>
+<!DOCTYPE HTML>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title><?=$title?></title>
-        
-        <!--JQuery-->
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        
-        <!--Materialize.css-->
-        <link rel="stylesheet" href="<?= $this->config->base_url()?>assets/materialize/css/materialize.css"/>
-        <!--<script src="<?= $this->config->base_url()?>assets/materialize/js/materialize.js"></script>-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        
-        
-        <!--"Document.ready scripts"-->
-        <script>
-            $( document ).ready(function(){
-                $(".button-collapse").sideNav();
-                $('.collapsible').collapsible();
-                $('.collapsible').collapsible({
-                    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-                  });
-            });
-        </script>
-        
-        <!--Font Awesome Icons-->
-        <link rel = "stylesheet" href = "<?= $this->config->base_url()?>assets/fontawesome/css/font-awesome.min.css">
-        
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href ="<?= $this->config->base_url()?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href ="<?= $this->config->base_url()?>assets/css/style.css" rel="stylesheet">
+        <link href="<?= $this->config->base_url()?>assets/color/default.css" rel="stylesheet">
+        <link rel="shortcut icon" href="<?= $this->config->base_url()?>images/img/favicon.ico">
     </head>
     <body>
-
