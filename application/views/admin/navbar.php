ae770279e21ece1a58cbc694a6416769c87d23f8
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2017-09-27 20:26:38 +0800

    PetDatabase Updated

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -28,8 +28,7 @@
                 </li>
 
                 <li class = "<?= $wholeUrl == $this->config->base_url()."admin" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-dashboard fa-2x"></i>Dashboard</a></li>
-                <li class = "<?= $wholeUrl == $this->config->base_url()."admin/petDatabase" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-database fa-2x"></i>Pet Database</a></li>
-                <li class = "<?= $wholeUrl == $this->config->base_url()."admin/adoptables" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-paw fa-2x"></i>Adoptables</a></li>    
+                <li class = "<?= $wholeUrl == $this->config->base_url()."admin/petDatabase" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-database fa-2x"></i>Pet Database</a></li>   
                 <li class = "<?= $wholeUrl == $this->config->base_url()."admin/reports" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-bar-chart fa-2x"></i>Reports</a></li>
                 <li class = "<?= $wholeUrl == $this->config->base_url()."admin/auditTrail" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#!"><i class="fa fa-list fa-2x"></i>Audit Trail</a></li>
                 <li class = "<?= $wholeUrl == $this->config->base_url()."admin/settings" ? "side-nav-active" : ""?>"><a class="waves-effect" href="#"><i class="fa fa-gear fa-2x"></i>Settings</a></li>
