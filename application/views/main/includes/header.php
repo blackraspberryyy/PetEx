db083b9ac1c87911c1acb4e5acd5c237391dd6d2
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2017-10-01 02:32:03 +0800

    Landing Page Changed

--- application/views/main/includes/header.php
+++ application/views/main/includes/header.php
@@ -7,23 +7,70 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <link href ="<?= $this->config->base_url()?>assets/maxim/css/bootstrap-responsive.css" rel="stylesheet">
-        <link href ="<?= $this->config->base_url()?>assets/maxim/css/style.css" rel="stylesheet">
-        <link href="<?= $this->config->base_url()?>assets/maxim/color/default.css" rel="stylesheet">
-        <link rel="shortcut icon" href="<?= $this->config->base_url()?>images/img/favicon.ico">
-        
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.scrollTo.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.nav.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.localscroll-1.2.7-min.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/bootstrap.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.prettyPhoto.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/isotope.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/jquery.flexslider.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/inview.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/animate.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/js/custom.js"></script>
-        <script src="<?= $this->config->base_url()?>assets/maxim/contactform/contactform.js"></script>
-        
+        <link href ="<?= $this->config->base_url() ?>assets/maxim/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href ="<?= $this->config->base_url() ?>assets/maxim/css/style.css" rel="stylesheet">
+        <link href ="<?= $this->config->base_url() ?>assets/maxim/css/animatePic.css" rel="stylesheet">
+        <link href="<?= $this->config->base_url() ?>assets/maxim/color/default.css" rel="stylesheet">
+        <link rel="shortcut icon" href="<?= $this->config->base_url() ?>images/img/petexIcon.ico">
+        <link  href ="<?= $this->config->base_url() ?>assets/fontawesome/css/font-awesome.min.css" rel ="stylesheet">
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.scrollTo.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.nav.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.localscroll-1.2.7-min.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/bootstrap.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.prettyPhoto.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/isotope.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/jquery.flexslider.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/inview.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/animate.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/js/custom.js"></script>
+        <script src="<?= $this->config->base_url() ?>assets/maxim/contactform/contactform.js"></script>
+
     </head>
+    <style>
+        .navImg{
+            height:40px;
+            max-width: 100%;
+            display:block;
+        }
+        .navbar .navbar-inner{
+            background: #388E3C ;
+            
+        }
+        .headerIcon{
+            height:180px;
+        }
+        
+        @media only screen and (max-width: 768px) {
+            .navImg {
+                margin-top: -10px;
+            }
+            .home-slide-content{
+                font-size:40px !important;
+            }
+        }
+    </style>
     <body>
+        <!-- navbar -->
+        <div class="navbar-wrapper">
+            <div class="navbar navbar-inverse navbar-fixed-top" >
+                <div class="navbar-inner">
+                    <div class="container">
+                        <!-- Responsive navbar -->
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>
+                        <h1 class="brand"><a href=""><img src = "<?= $this->config->base_url() ?>images/logo/logo.png" class="navImg"/></a></h1>
+                        <!-- navigation -->
+                        <nav class="pull-right nav-collapse collapse">
+                            <ul id="menu-main" class="nav">
+                                <li><a title="team" href="#about">About</a></li>
+                                <li><a title="services" href="#services">Services</a></li>
+                                <li><a title="works" href="#works">Works</a></li>
+                                <li><a title="blog" href="#blog">Blog</a></li>
+                                <li><a title="contact" href="#contact">Contact</a></li>
+                                <li><a href="page.html">Page</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
